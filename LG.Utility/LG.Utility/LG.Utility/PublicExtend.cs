37bb95a82d9537c8bb6fad0c123bec1c4fcdc8f6
aqiu <563644741@qq.com>
2014-11-12 14:06:25 +0800

    添加各种静态帮助方法
    添加各种静态帮助方法

--- LG.Utility/LG.Utility/LG.Utility/PublicExtend.cs
+++ LG.Utility/LG.Utility/LG.Utility/PublicExtend.cs
@@ -5,40 +5,18 @@ using System.Text;
 using System.Data;
 using System.Reflection;
 using System.Web;
+using System.IO;
+using System.Drawing;
+using System.Net;
 
 namespace LG.Utility {
     public static class PublicExtend {
         /// <summary>
-        /// 尽最大努力取得用户IP
+        /// 尽最大努力取得用户IP (转移到StaticFunctions，其他地方在调用，这里多写个冗余方法，防止报错)
         /// </summary>
         /// <returns>用户IP</returns>
         public static string GetClientRealIp() {
-            var Request = HttpContext.Current.Request;
-            if (Request == null) return "";
-            try {
-                string returnValue = "";
-                if (Request.ServerVariables["HTTP_VIA"] != null) {
-                    returnValue = Request.ServerVariables["HTTP_X_FORWARDED_FOR"].ToString();
-                }
-
-                //因为我们的调度服务器没有HTTP_VIA值，所以加入这段代码进行修复
-                if (string.IsNullOrEmpty(returnValue)) {
-                    if (Request.ServerVariables["HTTP_X_FORWARDED_FOR"] != null)
-                        returnValue = Request.ServerVariables["HTTP_X_FORWARDED_FOR"].ToString();
-                }
-
-                if (string.IsNullOrEmpty(returnValue)) {
-                    returnValue = HttpContext.Current.Request.UserHostAddress;
-                }
-
-                if (returnValue == "::1")
-                    returnValue = "127.0.0.1";
-
-                return returnValue;
-            } catch {
-                return "0.0.0.0";
-            }
-
+            return StaticFunctions.GetClientRealIp();
         }
         /// <summary>
         /// 集合转为符号隔开的字符串
@@ -51,16 +29,6 @@ namespace LG.Utility {
             if (datas == null || datas.Count <= 0) return string.Empty;
             return string.Join(separator, datas);
         }
-        /// <summary>
-        /// 获取随机种子码
-        /// </summary>
-        /// <returns></returns>
-        public static int GetRandSeed() {
-            byte[] bytes = new byte[8];
-            System.Security.Cryptography.RNGCryptoServiceProvider rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
-            rng.GetBytes(bytes);
-            return BitConverter.ToInt32(bytes, 0);
-        }
-
+      
     }
 }
--- LG.Utility/LG.Utility/LG.Utility/StaticFunctions.cs
+++ LG.Utility/LG.Utility/LG.Utility/StaticFunctions.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.IO;
 using System.Web;
 using System.Collections;
+using System.Net;
+using System.Drawing;
 
 namespace LG.Utility {
     public class StaticFunctions {
@@ -175,6 +177,112 @@ namespace LG.Utility {
             if(HttpContext.Current.Cache[key] != null) {
                 HttpContext.Current.Cache.Remove(key);
             }
+        }
+
+        /// <summary>
+        /// 获取随机种子码
+        /// </summary>
+        /// <returns></returns>
+        public static int GetRandSeed() {
+            byte[] bytes = new byte[8];
+            System.Security.Cryptography.RNGCryptoServiceProvider rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            rng.GetBytes(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        /// <summary>
+        /// 字节转img
+        /// </summary>
+        /// <param name="byt"></param>
+        /// <returns></returns>
+        public static Image ByteToImg(byte[] byt) {
+            MemoryStream ms = new MemoryStream(byt);
+            Image img = Image.FromStream(ms);
+            return img;
+        }
+
+        /// <summary>
+        /// 根据URL保存图片
+        /// </summary>
+        /// <param name="imgUrl">图片URL地址</param>
+        /// <param name="savePath">保存路径</param>
+        /// <param name="saveFileName">保存图片名，不包括拓展名</param>
+        /// <returns></returns>
+        public static bool SaveFile(string imgUrl, string savePath, string saveFileName) {
+            if (imgUrl.IsNullOrWhiteSpace()) return false;
+            if (!Directory.Exists(savePath)) Directory.CreateDirectory(savePath);
+            try {
+                using (var webClient = new WebClient()) {
+                    var uri = new Uri(imgUrl);
+                    if (uri == null) return false;
+                    var fileName = Path.GetFileName(imgUrl);
+                    var extension = Path.GetExtension(imgUrl);
+                    if (extension.Contains("?")) {
+                        var eNum = extension.IndexOf("?");
+                        extension = extension.Substring(0, eNum);
+                    }
+                    byte[] imgByte = webClient.DownloadData(imgUrl);
+                    if (imgByte == null || imgByte.Count() <= 0) return false;
+                    var ms = new MemoryStream(imgByte);
+                    if (ms == null) return false;
+                    string saveFilePath = savePath + saveFileName + extension;
+                    var newImg = System.Drawing.Image.FromStream(ms);
+                    if (newImg == null) return false;
+                    newImg.Save(saveFilePath);
+                    webClient.Dispose();
+                }
+            } catch {
+                return false;
+            }
+            return true;
+        }
+        /// <summary>
+        /// 尽最大努力取得用户IP
+        /// </summary>
+        /// <returns>用户IP</returns>
+        public static string GetClientRealIp() {
+            var Request = HttpContext.Current.Request;
+            if (Request == null) return "";
+            try {
+                string returnValue = "";
+                if (Request.ServerVariables["HTTP_VIA"] != null) {
+                    returnValue = Request.ServerVariables["HTTP_X_FORWARDED_FOR"].ToString();
+                }
+
+                //因为我们的调度服务器没有HTTP_VIA值，所以加入这段代码进行修复
+                if (string.IsNullOrEmpty(returnValue)) {
+                    if (Request.ServerVariables["HTTP_X_FORWARDED_FOR"] != null)
+                        returnValue = Request.ServerVariables["HTTP_X_FORWARDED_FOR"].ToString();
+                }
+
+                if (string.IsNullOrEmpty(returnValue)) {
+                    returnValue = HttpContext.Current.Request.UserHostAddress;
+                }
+
+                if (returnValue == "::1")
+                    returnValue = "127.0.0.1";
+
+                return returnValue;
+            } catch {
+                return "0.0.0.0";
+            }
+
+        }
+        /// <summary>
+        /// 输出客服端，下载文件
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="saveFileName"></param>
+        public static void OutClientToDownFile(string filePath, string saveFileName) {
+            HttpContext.Current.Response.Buffer = true;
+            HttpContext.Current.Response.Expires = 0;
+            HttpContext.Current.Response.ExpiresAbsolute = DateTime.Now.AddDays(-1);
+            HttpContext.Current.Response.AddHeader("pragma", "no-cache");
+            HttpContext.Current.Response.AddHeader("cache-control", "private");
+            HttpContext.Current.Response.Clear();
+            HttpContext.Current.Response.ContentType = "application/octet-stream";
+            HttpContext.Current.Response.AddHeader("Content-Disposition", "attachment;filename=" + saveFileName);
+            HttpContext.Current.Response.WriteFile(filePath);
         }
     }
 }
