fde0f7ba364e0d26397a4ae583ac0990562bd20e
aqiu <563644741@qq.com>
2014-11-14 11:49:49 +0800

    各种添加
    添加创建flash
    添加图片帮助
    添加Redis帮助

---
+++ LG.Utility/LG.Utility/LG.Utility/CreateVideoHelper.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using SwfDotNet.IO;
+using SwfDotNet.IO.Tags;
+using SwfDotNet.IO.Tags.Types;
+namespace LG.Utility {
+    public class CreateVideoHelper {
+        /// <summary>
+        /// 生成flash
+        /// </summary>
+        /// <param name="imgFilePaths"></param>
+        /// <param name="saveSwfFilePath"></param>
+        /// <returns></returns>
+        public static bool CreateSwf(List<string> imgFilePaths, string saveSwfFilePath) {
+            try {
+                Swf swf = new Swf();
+                foreach (var item in imgFilePaths) {
+                    if (!new FileInfo(item).Exists) continue;
+                    Image img = Image.FromFile(item);
+                    img = img.GetThumbnailImage(img.Width / 2, img.Height / 2, null, IntPtr.Zero);
+                    int posX = 0;
+                    int posY = 0;
+                    int imgWidth = img.Width;
+                    int imgHeight = img.Height;
+                    //Create a new Swf instance
+                    //Set size in inch unit (1 pixel = 20 inches)
+                    swf.Size = new Rect(0, 0, (posX + imgWidth) * 20, (posY + imgHeight) * 20);
+                    swf.Version = 7;  //Version 7 (for compression, must be > 5)
+                    swf.Header.Signature = "CWS";  //Set the signature to compress the swf
+                    //Set the background color tag as white
+                    swf.Tags.Add(new SetBackgroundColorTag(255, 255, 255));
+                    //Set the jpeg tag
+                    ushort jpegId = swf.GetNewDefineId();
+                    //Load the jped directly from an image
+                    //In fact, this line will load the jpeg data in the file as 
+                    //a library element only (not to display the jpeg)
+                    swf.Tags.Add(DefineBitsJpeg2Tag.FromImage(jpegId, img));
+                    //Now we will define the picture's shape tag
+                    //to define all the transformations on the picture 
+                    //(as rotation, color effects, etc..) 
+                    DefineShapeTag shapeTag = new DefineShapeTag();
+                    shapeTag.CharacterId = swf.GetNewDefineId();
+                    shapeTag.Rect = new Rect(posX * 20 - 1, posY * 20 - 1,
+                         (posX + imgWidth) * 20 - 1, (posY + imgHeight) * 20 - 1);
+                    FillStyleCollection fillStyles = new FillStyleCollection();
+                    fillStyles.Add(new BitmapFill(FillStyleType.ClippedBitmapFill,
+                          ushort.MaxValue, new Matrix(0, 0, 20, 20)));
+                    fillStyles.Add(new BitmapFill(FillStyleType.ClippedBitmapFill,
+                                   jpegId, new Matrix(posX * 20 - 1, posY * 20 - 1,
+                                   (20.0 * imgWidth) / img.Width,
+                                   (20.0 * imgHeight) / img.Height)));
+                    LineStyleCollection lineStyles = new LineStyleCollection();
+                    ShapeRecordCollection shapes = new ShapeRecordCollection();
+                    shapes.Add(new StyleChangeRecord(posX * 20 - 1, posY * 20 - 1, 2));
+                    shapes.Add(new StraightEdgeRecord(imgWidth * 20, 0));
+                    shapes.Add(new StraightEdgeRecord(0, imgHeight * 20));
+                    shapes.Add(new StraightEdgeRecord(-imgWidth * 20, 0));
+                    shapes.Add(new StraightEdgeRecord(0, -imgHeight * 20));
+                    shapes.Add(new EndShapeRecord());
+                    shapeTag.ShapeWithStyle =
+                       new ShapeWithStyle(fillStyles, lineStyles, shapes);
+                    swf.Tags.Add(shapeTag);
+
+                    //Place the picture to the screen with depth=1
+                    swf.Tags.Add(new PlaceObject2Tag(shapeTag.CharacterId, 1, 0, 0));
+                    swf.Tags.Add(new ShowFrameTag());
+                    swf.Tags.Add(new RemoveObject2Tag(1)); //THE ADDED LINE!!//
+                    img.Dispose();
+                }
+                //Write the swf to a file
+                SwfWriter writer = new SwfWriter(saveSwfFilePath);
+                writer.Write(swf);
+                writer.Close();
+                return true;
+            } catch {
+                return false;
+            }
+        }
+    }
+}
---
+++ LG.Utility/LG.Utility/LG.Utility/RedisHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.Redis;
+
+namespace LG.Utility {
+    /// <summary>
+    /// Redis 帮助类
+    /// </summary>
+    public class RedisHelper {
+        private string _RedisConnection { get; set; }
+        private string _RedisConnectionAuth { get; set; }
+        public RedisHelper(string redisConnection, string redisConnectionAuth = "") {
+            _RedisConnection = redisConnection;
+            _RedisConnectionAuth = redisConnectionAuth;
+        }
+        private static PooledRedisClientManager _pcm;
+        private  PooledRedisClientManager GetDefaultManager() {
+            if (_pcm != null) return _pcm;
+            _pcm = new PooledRedisClientManager(new[] { _RedisConnection });
+            _pcm.ConnectTimeout = 1000;
+            _pcm.IdleTimeOutSecs = 30;
+            return _pcm;
+        }
+        /// <summary>
+        /// 获取链接redis对象
+        /// </summary>
+        /// <returns></returns>
+        public  IRedisClient GetClient() {
+            IRedisClient client = GetDefaultManager().GetClient();
+            if (!_RedisConnectionAuth.IsNullOrWhiteSpace()) client.Password = _RedisConnectionAuth;
+            return client;
+        }
+        #region "帮助拓展"
+        #region "字符串类型"
+        /// <summary>
+        /// 获取Redis存储类型为string 的数据 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public T Get<T>(IRedisClient client,string key) where T : class {
+            var isHave = client.ContainsKey(key);
+            if (isHave) return client.Get<T>(key);
+            return default(T);
+        }
+        /// <summary>
+        /// 获取Redis存储类型为string 的数据 ,如果不存在就用函数返回的值,自动加入缓存.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="fun"></param>
+        /// <param name="expiresAt"></param>
+        /// <returns></returns>
+        public  T Get<T>(IRedisClient client,string key, Func<T> fun, DateTime expiresAt) where T : class {
+            var chacheData = Get<T>(client,key);
+            if (chacheData != null) return chacheData;
+            var saveData = fun();
+            client.Set<T>(key, saveData, expiresAt);
+            return saveData;
+        }
+        /// <summary>
+        /// 移除key对应的Redis缓存
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(IRedisClient client, string key) {
+            client.Remove(key);
+        }
+        #endregion
+        #region "哈希类型"
+        /// <summary>
+        /// 获取hashvalue（值），根据key 和field(字段)
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="field"></param>
+        /// <returns></returns>
+        public string GetHash(IRedisClient client, string key, string field) {
+            var isHave = client.ContainsKey(key);
+            if (!isHave) return string.Empty;
+            return client.GetValueFromHash(key, field);
+        }
+
+        public bool SetHash(IRedisClient client,string key,string field,string value) {
+            return client.SetEntryInHash(key, field, value);
+        }
+        #endregion
+        #endregion
+    }
+}
